eb8c22b0b6eb20c58578dfcbfe6adb6b72080b58
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-07 17:00:41 -0400

    js-functions-lab

--- app.js
+++ app.js
@@ -56,3 +56,45 @@ console.log(sumArray);
 
 
 Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]); would return 40.
+
+
+
+function multiplyArray([1, 6, 4]);
+{return 1 * 6 * 4};
+console.log(multiplyArray);
+
+
+Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
+
+const numArgs = function() {
+
+    return arguments.length;
+};
+
+
+Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar'); would return the string "ratskcor".
+
+
+function reverseString('ryan'); {
+    return reverseString;
+    console.log(reverseString)
+};
+
+
+Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
+
+const longestStringInArray = function() {
+return longest.length;
+
+}
+
+
+Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+
+function stringsLongerThan(['salad', 'veggies', 'fruits'], 2); {
+
+
+return stringsLongerThan;
+console.log(stringsLongerThan)
+
+};
